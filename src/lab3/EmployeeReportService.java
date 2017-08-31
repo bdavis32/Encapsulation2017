ed1ce5f94bec3562b68ae915ac25580a6676bf5a
Benjamin <Benjamin@10.30.18.236>
2017-08-31 15:43:32 -0500

    Set up the EmployeeReportService class to take the responsibility of console output.

--- src/lab3/EmployeeReportService.java
+++ src/lab3/EmployeeReportService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab3;
 
 /**
@@ -10,5 +5,20 @@ package lab3;
  * @author Benjamin
  */
 public class EmployeeReportService {
+    private String report = "";
     
+    //Add some data to the "report"
+    public void addData(String data){
+        report += data;
+    }
+    
+    //Display the "report" to the console
+    public void outputReport(){
+        System.out.println(report);
+    }
+    
+    //Clear the report
+    public void clearReport(){
+        report = "";
+    }
 }
