f6a5383d0d998f3eb4d3d448395ffa252a2fe9f5
Benjamin <Benjamin@10.30.18.236>
2017-08-31 15:11:37 -0500

    Declared all properties (not Boolean) private and added Getters & Setters (for Lab1)

--- src/lab1/Employee.java
+++ src/lab1/Employee.java
@@ -4,24 +4,67 @@ import java.util.Date;
 
 /**
  * Fix the code in this class to do PROPERTY encapsulation correctly. Also
- * consider if any of the properties should be mandatory and use a 
- * constructor to enforce that. Review the tips in the document 
- * "EncapCheckList.pdf" if needed.
+ * consider if any of the properties should be mandatory and use a constructor
+ * to enforce that. Review the tips in the document "EncapCheckList.pdf" if
+ * needed.
  *
- * @author      Jim Lombardo, WCTC Instructor
- * @version     1.02
+ * @author Jim Lombardo, WCTC Instructor
+ * @version 1.02
  */
 public class Employee {
-    String firstName;
-    String lastName;
-    public String ssn;
+
+    private String firstName;
+    private String lastName;
+    private String ssn;
+
+    //These do not need private or public methods. 
+    //There's only two values for Boolean properties: 0 (false) or 1 (true)
     boolean metWithHr;
     boolean metDeptStaff;
     boolean reviewedDeptPolicies;
     boolean movedIn;
-    String cubeId;
-    Date orientationDate;
+    //End of Boolean
+    private String cubeId;
+    private Date orientationDate;
 
+    //Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getCubeId() {
+        return cubeId;
+    }
+
+    public void setCubeId(String cubeId) {
+        this.cubeId = cubeId;
+    }
+
+    public Date getOrientationDate() {
+        return orientationDate;
+    }
+
+    public void setOrientationDate(Date orientationDate) {
+        this.orientationDate = orientationDate;
+    }
 }
